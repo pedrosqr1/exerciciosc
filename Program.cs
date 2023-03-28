e623a838cc18920c272a4caaa278b3acb253421d
pedrosqr1 <127567823+pedrosqr1@users.noreply.github.com>
2023-03-27 21:11:56 -0300

    ex2

--- Program.cs
+++ Program.cs
@@ -1,23 +1,47 @@
 ﻿using System;
 
-namespace ex1
+namespace ex2
 {
     class Program
     {
         static void Main(string[] args)
+
+        //2. Escreva um algoritmo que lê três valores para os lados de um triângulo. O algoritmo deve verificar se o triângulo é equilátero(todos os lados iguais), isósceles(dois lados iguais) ou scaleno(todos lados diferentes).
+
         {
-            float numero;
-            Console.WriteLine("Insira um valor que esteja entre a faixa de 1 a 9: ");
-            numero = Convert.ToSingle(Console.ReadLine());
 
-            if ((numero < 1) || (numero > 9))
+            float lado1;
+            float lado2;
+            float lado3;
+
+            Console.WriteLine("Digite o valor do lado 1: ");
+            lado1 = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor do lado 2: ");
+            lado2 = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor do lado 3: ");
+            lado3 = Convert.ToSingle(Console.ReadLine());
+
+            if ((lado1 == lado2) && (lado1 == lado3))
             {
-                Console.WriteLine("O valor está fora da faixa permitida.");
-
+                Console.WriteLine("O triângulo é equilátero.");
             }
-            else
+            if ((lado1 != lado2) && (lado2 != lado3) && (lado3 != lado1))
             {
-                Console.WriteLine("O valor está na faixa permitida.");
+                Console.WriteLine("O triangulho é scaleno.");
+            }
+            if ((lado1 == lado2) && (lado1 != lado3))
+            {
+                Console.WriteLine("O triângulo é isóceles.");
+            }
+            if ((lado2 == lado3) && (lado1 != lado2))
+            {
+                Console.WriteLine("O triângulo é isóceles.");
+            }
+            if ((lado1 == lado3) && (lado1 != lado2))
+            {
+                Console.WriteLine("O triângulo é isóceles.");
             }
 
         }
